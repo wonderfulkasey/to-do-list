809be50466ad97420a2695a62a1dc2b5c4ee597f
wonderfulkasey <kaseyarnold15@gmail.com>
2020-11-12 15:30:27 -0500

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const todo = ["go for a walk", "go to store", "eat human food"]
+
+const list = document.getElementById("list");
+
+function addData() {
+    for(let i = 0; i < todo.length; i++) {
+        newElement(todo[i])
+    }
+
+}
+
+function newElement(item) {
+    const li = document.createElement("li");
+    li.innerText = item
+    list.appendChild(li)
+    
+}
+
+addData()
