df5e064c5cf92ef159ed110f31a9d367ec45e42a
wonderfulkasey <kaseyarnold15@gmail.com>
2020-11-12 19:55:41 -0500

    added event listener

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const todo = ["go for a walk", "go to store", "eat human food"]
 
 const list = document.getElementById("list");
 
+
+document.addEventListener("submit", newElement);
+
 function addData() {
     for(let i = 0; i < todo.length; i++) {
         newElement(todo[i])
@@ -9,11 +12,13 @@ function addData() {
 
 }
 
+
 function newElement(item) {
     const li = document.createElement("li");
     li.innerText = item
     list.appendChild(li)
     
 }
+
 
 addData()
